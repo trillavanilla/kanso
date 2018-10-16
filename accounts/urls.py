f3cb728a3b98c505d9900967123f3f74cf11d2de
Olivia Hansell <trillav@olivias-mbp.home>
2018-10-16 11:20:39 +0100

    trying again

---
+++ accounts/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.home),
+    url(r'^login/$', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    url(r'^logout/$', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^profile/$', views.view_profile, name='view_profile'),
+    url(r'^profile/edit$', views.edit_profile, name='edit_profile'),
+    url(r'^change-password/$', views.change_password, name='change_password'),
+    url(r'^reset-password/$', PasswordResetView.as_view(), name='reset_password'),
+    url(r'^reset-password/done/$', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>,+)/$',
+    PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^reset-password/complete/$', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+]
